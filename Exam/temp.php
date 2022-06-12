cee8b79b2c60714e2d758d0fb5fff3e570359d44
yvulcheva <yvulcheva@gmail.com>
2022-06-12 18:26:26 +0300

    Changes

---
+++ Exam/temp.php
@@ -0,0 +1,13 @@
+<?php
+    include "../MainPage/array.php";
+
+    echo $array[0];
+    
+    echo $array[0]['Question'];
+
+    // for($i = 0; $i < count($array); $i++){
+            //     foreach($array[$i] as $value){
+            //         echo '<pre>'; print_r($value); echo '</pre>';
+            //     }
+            // }
+?>
