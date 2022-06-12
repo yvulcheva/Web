b768aaecd1e0c46ffe25020b22c29cff009c81e9
Ivaylo Belchev <ivailobelchev2000@gmail.com>
2022-06-13 00:18:10 +0300

    Changed to Exam and Example.csv

--- Exam/exam1.php
+++ Exam/exam1.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="exam.css">
+    <link rel="stylesheet" href="examStyle.css">
     <title>Exam</title>
 </head>
 
@@ -18,9 +18,11 @@
             $score = 0;
             for($i = 0; $i < count($array); $i++)
             {
-                ?>
-                <form action="score.php" method="post">
-                    <?php
+
+                // Post is needed to send the data to the next page
+                echo '<form action="score.php" method="post">';
+
+                    // Printing the questions
                     echo "<ul class='questions'>";
                         echo "<li>";
                             print_r("Номер: ");
@@ -52,8 +54,9 @@
                         echo "</li>";
                     echo "</ul>";
                     $varQuestion = "question" . $i;
-                    var_dump($varQuestion); // comment this later
+                    // var_dump($varQuestion);
 
+                    // Select
                     echo '<select id="' . $varQuestion . '" name="' . $varQuestion . '">';
                         echo '<option value="0">Изберете отговор</option>';
                         echo '<option value="1">A</option>';
@@ -61,30 +64,13 @@
                         echo '<option value="3">C</option>';
                         echo '<option value="4">D</option>';
                     echo '</select>';
-
             }
-                ?>
 
-            <input type="submit" name="submit" value="Проверка">
-            </form>
-
-                <?php
-
-            function submit()
-            {
-                if(isset($_POST['submit'])){
-                    if(!empty($_POST['question'])) {
-                      $selected = $_POST['question'];
-                      print_r('You have chosen: ' . $selected);
-                    } else {
-                      print_r('Please select the value.');
-                    }
-                  }
-            }
+            echo '<input type="submit" name="submit" value="Проверка">';
+            echo '</form>';
         ?>
-        
 
-</section>
+    </section>
 
 </body>
 
--- Exam/score.php
+++ Exam/score.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="exam.css">
-    <title>Exam</title>
+    <link rel="stylesheet" href="examStyle.css">
+    <title>Exam score</title>
 </head>
 
 <body>
-    <section class="page">
+    <section class="pageScore">
         <h1>Изпитен режим</h1>
+        <br>
 
         <?php
             include "../MainPage/array.php";
@@ -22,18 +23,53 @@
             {
                 $varQuestion = "question" . $i;
                 $currentAnswer = $_POST[$varQuestion];
-                var_dump($currentAnswer);
+                //var_dump($currentAnswer);
 
                 if ($currentAnswer == $array[$i]['Answer'])
                 {
                     $score++;
+                    echo("<p>");
+                    print_r("Въпрос <b>" . ($i+1) . "</b> e верен!");
+                    echo("</p>");
+                }
+                else
+                {
+                    echo ("<p>");
+                    print_r("Въпрос <b>" . ($i+1) . "</b> e грешен! Верният отговор е <b>");
+                    switch ($array[$i]['Answer']) 
+                    {
+                        case 1:
+                            print_r("A.");
+                            break;
+                        case 2:
+                            print_r("B.");
+                            break;
+                        case 3:
+                            print_r("C.");
+                            break;
+                        case 4:
+                            print_r("D.");
+                            break;
+                    }
+                    echo("</b></p>");
                 }
             }
             
-            print_r("Score: ");
+            echo("<br><p><b>");
+            print_r("Резултат: ");
             print_r($score);
-            
+            print_r(" от " . count($array));
+            echo("</b></p><p><b>");
+            print_r("Оценка: " . ($score/(count($array))*100) . "%");
+            echo("</b></p>");
         ?>
+
+        <form action="exportResult.php">
+            <section id="buttons", class="buttons">
+                <button id="button" class="button">Експорт на Вашия резултат</button>
+            </section>
+        </form>
+        <br>
     </section>
     <!-- направи изход с всичките въпроси, отговори и дадените отговори -->
 </body>
